c6a2607ff9e1ee56c2f7865aab5e3cf5952368b3
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-04-07 00:43:30 +0530

    Update script.js

--- Advanced_Digital_Anolog_clock/script.js
+++ Advanced_Digital_Anolog_clock/script.js
@@ -1,1 +1,16 @@
+let hr = document.querySelector('#hr');
+let mn = document.querySelector('#mn');
+let sc = document.querySelector('#sc');
 
+setInterval(()=>{
+    
+let day = new Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * 6;
+let ss = day.getSeconds() * 6;
+
+hr.style.transform = `rotateZ(${hh + (mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+
+},1000)
