782b10b7cf4d21f8561941fcad7c3e4e975785bd
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-04-10 21:24:23 +0530

    Add files via upload

---
+++ Nine_Dot_Menu/script.js
@@ -0,0 +1,7 @@
+let navigation = document.querySelector('.navigation');
+let outerbodyReset = document.querySelector('.resetbody');
+navigation.onclick = function(){
+    navigation.classList.toggle('active')
+    
+}
+
